5fae8ddc9d4d1cd9cf65146277489ffec38eb5ad
Pyunghyuk Yoo <yoophi@gmail.com>
2016-04-14 22:48:56 +0900

    mock login/logout

--- src/guestbook/handler.clj
+++ src/guestbook/handler.clj
@@ -6,7 +6,12 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [guestbook.routes.home :refer [home-routes]]
-            [guestbook.models.db :as db]))
+            [guestbook.routes.auth :refer [auth-routes]]
+            [guestbook.models.db :as db]
+            [noir.session :as session]
+            [ring.middleware.session.memory :refer [memory-store]]))
+
+
 
 (defn init []
   (println "guestbook is starting")
@@ -21,6 +26,10 @@
   (route/not-found "Not Found"))
 
 (def app
-  (-> (routes home-routes app-routes)
-      (handler/site)
-      (wrap-base-url)))
+  (->
+   (handler/site
+    (routes auth-routes
+            home-routes
+            app-routes))
+   (session/wrap-noir-session {:store (memory-store)})))
+  
